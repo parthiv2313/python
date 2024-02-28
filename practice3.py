99b49ec079da3c3629559c0c6f56d13933752855
Your Name <you@example.com>
2024-02-28 15:56:21 +0530

    28-02-24

---
+++ practice3.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number: "))
+if (num % 2) == 0:
+   print("num EVEN ",num)
+else:
+   print("num is Odd",num)
---
+++ practice4.py
@@ -0,0 +1,7 @@
+n = int(input("Input a number: "))
+
+
+sum = (n * (n + 1)) / 2
+
+
+print("Sum of the first", n, "positive integers:", sum)
